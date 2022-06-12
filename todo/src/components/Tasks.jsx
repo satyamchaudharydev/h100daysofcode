dc1982dd053d79ead77f1d01ba84f594dd24cfb8
satyam <satyamchaudharydev@gmail.com>
2022-06-12 21:09:08 +0530

    trying to add animation to the container

--- todo/src/components/Tasks.jsx
+++ todo/src/components/Tasks.jsx
@@ -1,37 +1,38 @@
 import React from "react";
 
-
 import Task from "./Task";
 import { useTask } from "../context/Task";
 import { Reorder, AnimatePresence } from "framer-motion";
+import { LayoutGroup } from "framer-motion";
 
 function Tasks() {
   const { tasks, setTasks } = useTask();
- 
+
   return (
-    <Reorder.Group
-      axis="y"
-      as="ul"
-      layoutScroll
-      className="tasks"
-      values={tasks}
-      onReorder={setTasks}
-    >
-      <AnimatePresence initial="false">
-        {tasks.map((task, index) => {
-          console.log(task.id);
-          return (
-            <Task
-              key={task.id}
-              index={index}
-              task={task}
-              tasks={tasks}
-            ></Task>
-          );
-        })}
-      </AnimatePresence>
-      
-    </Reorder.Group>
+    <LayoutGroup>
+      <Reorder.Group
+        axis="y"
+        as="ul"
+        layoutScroll
+        layout
+        className="tasks"
+        values={tasks}
+        onReorder={setTasks}
+      >
+        <AnimatePresence initial="false">
+          {tasks.map((task, index) => {
+            return (
+              <Task
+                key={task.id}
+                index={index}
+                task={task}
+                tasks={tasks}
+              ></Task>
+            );
+          })}
+        </AnimatePresence>
+      </Reorder.Group>
+    </LayoutGroup>
   );
 }
 
